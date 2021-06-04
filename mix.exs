7febd5a1b0c8fdfa2956f97c7b3e7a4783e439de
Chase <chase.gilliam@gmail.com>
2021-06-04 13:26:03 -0400

    add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule CrunchBerry.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -21,8 +24,9 @@ defmodule CrunchBerry.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_live_view, "~> 0.15.7"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
