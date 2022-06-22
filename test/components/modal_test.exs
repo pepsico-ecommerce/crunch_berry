51b4c0357671cc78c4bac12287adbc60724c185d
Kelly Felkins <kelly@restlater.com>
2022-06-22 10:26:46 -0700

    an attempt to test the modal close action with live_isolated

--- test/components/modal_test.exs
+++ test/components/modal_test.exs
@@ -51,4 +51,50 @@ defmodule CrunchBerry.Components.ModalTest do
              |> Floki.attribute("class") == ["cancel_icon-classes"]
     end
   end
+
+  describe "closing the modal" do
+    defmodule ModalComponent do
+      use Phoenix.LiveComponent
+
+      def render(assigns) do
+        ~H"""
+        <div id="the_modal">
+          <div>The Modal</div>
+        </div>
+        """
+      end
+    end
+
+    defmodule LiveViewFixture do
+      use Phoenix.LiveView
+      alias CrunchBerry.Components.LiveHelpers
+
+      def mount(_params, _session, socket) do
+        {:ok, socket}
+      end
+
+      def render(assigns) do
+        ~H"""
+        <div id="the_live_view">
+          <div>The LiveView</div>
+          <%= LiveHelpers.live_modal(ModalComponent, return_to: "/") %>
+        </div>
+        """
+      end
+    end
+
+    test "with return_to defined, returns to return_to", %{conn: conn} do
+      {:ok, view, html} = live_isolated(conn, LiveViewFixture)
+
+      assert html =~ "The LiveView"
+      assert html =~ "The Modal"
+
+      html =
+        view
+        |> element("a", "×")
+        |> render_click()
+
+      refute html =~ "The Modal"
+    end
+  end
 end
