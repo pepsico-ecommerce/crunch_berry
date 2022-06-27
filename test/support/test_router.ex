02052b2e4202d22ca74d24f304d54ad8f9a4fc72
Mike Fisher <michael.fisher@pepsico.com>
2022-06-27 13:17:03 -0400

    add test router harness, improve endpoint test harness

---
+++ test/support/test_router.ex
@@ -0,0 +1,26 @@
+defmodule CrunchBerry.Router do
+  @moduledoc """
+  Router test harness for testing LiveViews
+  """
+  use Phoenix.Router
+  import Phoenix.LiveView.Router
+
+  pipeline :setup_session do
+    plug Plug.Session,
+      store: :cookie,
+      key: "_live_view_key",
+      signing_salt: "/VEDsdfsffMnp5"
+
+    plug :fetch_session
+  end
+
+  pipeline :browser do
+    plug :setup_session
+    plug :accepts, ["html"]
+    plug :fetch_live_flash
+  end
+
+  scope "/", CrunchBerry.LiveViewTest do
+    pipe_through [:browser]
+  end
+end
